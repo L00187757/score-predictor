0738145d86ed77aafd7d725b045671c55d8c3a63
L00187757 <l00187757@atu.ie>
2025-04-30 11:50:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,24 @@
 import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# Load dataset
+df = pd.read_csv('weight-height.csv')  # Download from Kaggle
+
+# Prepare data
+X = df[['Height']]
+y = df['Weight']
+
+# Train model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Save model
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+import pandas as pd
 import numpy as np
 import pickle
 from sklearn.model_selection import train_test_split
